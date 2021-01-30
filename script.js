3c68901da0c3d45fa4828a00fafc30d72882cd38
Blanco Augusto <blanco.augusto.r@gmail.com>
2021-01-30 12:57:35 -0300

    Add checkbox

--- script.js
+++ script.js
@@ -10,8 +10,19 @@ btn_todo.addEventListener('click', (e) => {
   const list_element = document.createElement('li')
   list_element.setAttribute('id', 'list-element')
   list_element.setAttribute('class', 'list-element')
-  list_element.innerText = input_todo.value
-  if(list_element.textContent != '') list_todo.appendChild(list_element);
+  const text_input = input_todo.value
+  list_element.textContent = text_input
+  const checkbox_element = document.createElement('input')
+  checkbox_element.setAttribute('type', 'checkbox')
+  checkbox_element.setAttribute('id', 'chekbox-element')
+  checkbox_element.setAttribute('class', 'chekbox-element')
+  list_element.appendChild(checkbox_element)
+  
+  if(list_element.textContent != '') {
+    list_todo.appendChild(list_element)
+  }
+
   input_todo.value = ''
 
+  console.log(list_element)
 })
